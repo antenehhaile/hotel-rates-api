172eb660764d0c50dfab9ada9ed6070ece715ced
Anteneh Haile <anteneh@Antenehs-MacBook-Pro.local>
2024-02-25 17:14:59 -0500

    Updated the build script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o ./app/hotel-rates-api ./
-tar -czvf rates-api.tar.gz ./hotel-rates-api.tar.gz
+tar -czvf rates-api.tar.gz ./hotel-rates-api

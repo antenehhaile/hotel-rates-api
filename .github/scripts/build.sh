32878b279f44ab99781da1ccb8320fb2e32bf165
Anteneh Haile <anteneh@Antenehs-MacBook-Pro.local>
2024-02-25 14:23:30 -0500

    Updated the build script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -xe
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" . -o /app/hotel-rates-api
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o ./app/hotel-rates-api ./
